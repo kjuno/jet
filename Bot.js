54edec93ca241f40e904d99144323e6123159d4b
kjuno <34090514+kjuno@users.noreply.github.com>
2017-12-31 16:19:38 +0100

    added !prefix and !server

--- Bot.js
+++ Bot.js
@@ -5,6 +5,13 @@ var prefix = '!';
 var fs = require('fs');
 var json = JSON.parse(fs.readFileSync('./token.json', 'utf8'));
 
+function embed(string, color){
+    var embed = new Discord.RichEmbed();
+    embed.setDescription(string);
+    embed.setColor(color);
+    return embed;
+}
+
 client.on('ready', () => {
   console.log(`Bot ist einsatzbereit! ${client.user.username}`);
 });
@@ -13,12 +20,17 @@ client.on('message', msg => {
     if(msg.author == client.user) return;
 
     var content = msg.content.toString();
-    var cmd = content.split(' ')[0];
+    var args = content.split(' ', 2);
+    var cmd = args[0];
     var channel = msg.channel;
 
     switch(cmd){
-        case`${prefix}ping`:
-            channel.send('Pong!');
+        case`${prefix}server`:
+            channel.send(embed(`Servername: ${msg.guild.name}`, 0x3adb1e));
+            break;
+        case`${prefix}prefix`:
+            prefix = args[1];
+            channel.send(`Prefix wurde in \"${prefix}\" geändert!`)
             break;
     }
 });
